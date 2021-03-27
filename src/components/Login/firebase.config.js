7d09ce0a92305b9eb3282661e9fb09ab5dc1b60f
Sarwar-Putul <sarwarputul@gmail.com>
2021-03-28 02:15:16 +0600

    after server added

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfHlol5vg6dst3BvrN5GN9HLsJ3tUz_DI",
+  authDomain: "burj-al-arab-e2295.firebaseapp.com",
+  projectId: "burj-al-arab-e2295",
+  storageBucket: "burj-al-arab-e2295.appspot.com",
+  messagingSenderId: "498359702170",
+  appId: "1:498359702170:web:477a2432effb1dac0c4523"
+  };
+  export default firebaseConfig;
